6e99d615bc1c0b070367dec15fbc9f24c8fb0f3d
Abhishek kumar <abhishekkumar702770@gmail.com>
2024-09-26 12:55:09 +0530

    my program

---
+++ Untitled1.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+	int i=;
+	double sum=0,temp;
+	
+	for(i=1;i<=25;i++);
+	 temp=1/(pow(i,2));
+	 sum=sum + temp;
+}
+ printf("sum to 25 terms")
---
+++ coding.cpp
@@ -0,0 +1,8 @@
+#include"stdio.h"
+  int main(){
+  	int a=3;
+  	printf("%d",scanf("%d%c",&a));
+  	//return0;
+  }
+  
+  	  
